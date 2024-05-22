faf23e5fb74a478289de952689996134a34da40c
CristianLopes <cristianqibins@hotmail.com>
2024-05-21 22:29:53 -0300

    setup prisma orm

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,20 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.$connect().then(() => {
+  console.log('prisma connected ')
+  //   prisma.user
+  //     .create({
+  //       data: {
+  //         name: 'Cristian Diego',
+  //         email: 'cristian@gmail.com',
+  //       },
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //     })
+})
