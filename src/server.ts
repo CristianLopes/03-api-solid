c83e1c03a9428e810e8884eadcf4add5252c5138
CristianLopes <cristianqibins@hotmail.com>
2024-05-21 22:00:53 -0300

    add environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('🚀 Server Running!')
+    console.log(`🚀 Server Running! env: ${env.NODE_ENV}`)
   })
